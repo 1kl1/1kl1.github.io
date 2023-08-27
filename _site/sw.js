77b2d9591568aecb7c2cae86bf6d58d55243fe9f
1kl1 <mamy0320@naver.com>
2023-08-27 21:32:11 +0900

    commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230827.141602';
+const cacheName = 'chirpy-20230827.144129';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
